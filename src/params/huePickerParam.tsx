36361adcdb713ead3c65d6b1d53c1379dcf12568
MikeyBurkman <michael.burkman@gmail.com>
2021-08-26 11:41:58 -0400

    Hue picker param

---
+++ src/params/huePickerParam.tsx
@@ -0,0 +1,57 @@
+import { Icon, Stack, Tooltip, Typography } from '@material-ui/core';
+import React from 'react';
+import { HuePicker } from 'react-color';
+import * as convert from 'color-convert';
+
+import { ParamValue, ParamFunction } from '../domain/types';
+import { toHexColor } from '../domain/utils';
+
+const HuePickerParam: React.FC<{
+  name: string;
+  value?: number;
+  description?: string;
+  onChange: (v: ParamValue<number>) => void;
+}> = ({ name, value, description, onChange }) => {
+  const hexColor =
+    value === undefined
+      ? undefined
+      : toHexColor([...convert.hsl.rgb([value, 255, 255]), 255]);
+  return (
+    <Stack spacing={1}>
+      <Stack direction="row" spacing={1}>
+        <Typography variant="body2">{name}</Typography>
+        {description && (
+          <Tooltip title={description}>
+            <Icon fontSize="small">help</Icon>
+          </Tooltip>
+        )}
+      </Stack>
+      <HuePicker
+        color={hexColor}
+        onChangeComplete={({ hsl }) => onChange({ valid: true, value: hsl.h })}
+      />
+    </Stack>
+  );
+};
+
+export function huePickerParam(args: {
+  name: string;
+  defaultValue?: number;
+  description?: string;
+}): ParamFunction<number> {
+  return {
+    name: args.name,
+    defaultValue: args.defaultValue
+      ? { valid: true, value: args.defaultValue }
+      : { valid: false },
+    fn: (params) => {
+      return (
+        <HuePickerParam
+          name={args.name}
+          value={params.value.valid ? params.value.value : undefined}
+          onChange={params.onChange}
+        />
+      );
+    },
+  };
+}
--- src/transforms/hue-shift.ts
+++ src/transforms/hue-shift.ts
@@ -1,20 +1,19 @@
 import { buildTransform } from '../domain/types';
 import { fromHexColor, mapImage } from '../domain/utils';
 import * as convert from 'color-convert';
-import { colorPickerParam } from '../params/colorPickerParam';
+import { huePickerParam } from '../params/huePickerParam';
 
 export const hueShift = buildTransform({
   name: 'Hue Shift',
   description: 'Shift the hue to some other color',
   params: [
-    colorPickerParam({
+    huePickerParam({
       name: 'Hue',
-      defaultValue: fromHexColor('#000000'),
+      defaultValue: 128,
     }),
   ],
   fn: mapImage(({ coord, getSrcPixel, parameters }) => {
-    const [newColor] = parameters;
-    const [newHue] = convert.rgb.hsl([newColor[0], newColor[1], newColor[2]]);
+    const [newHue] = parameters;
     const [r, g, b, a] = getSrcPixel(coord);
     const [, s, l] = convert.rgb.hsl(r, g, b);
     const [newR, newG, newB] = convert.hsl.rgb([newHue, s, l]);
