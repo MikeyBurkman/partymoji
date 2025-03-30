419698ef159013413146793199a0bef75ca07589
Tom McKearney <tommck@gmail.com>
2025-03-30 12:19:10 -0400

    updating switching hue implementation

--- src/params/huePickerParam.tsx
+++ src/params/huePickerParam.tsx
@@ -1,7 +1,7 @@
 import { Stack, Typography } from '@material-ui/core';
 import * as convert from 'color-convert';
-import React from 'react';
-import { HuePicker } from 'react-color';
+import React, { useEffect } from 'react';
+import ColorPicker, { useColorPicker } from 'react-best-gradient-color-picker';
 import { HelpTooltip } from '~/components/HelpTooltip';
 import type { ParamFnDefault, ParamFunction } from '~/domain/types';
 import { colorUtil } from '~/domain/utils';
@@ -9,27 +9,43 @@ import { toParamFunction } from './utils';
 
 const HuePickerParam: React.FC<{
   name: string;
-  value?: number;
+  value: number;
   description?: string;
   onChange: (v: number) => void;
 }> = ({ name, value, description, onChange }) => {
   const hexColor = React.useMemo(
-    () =>
-      value === undefined
-        ? undefined
-        : colorUtil.toHexColor([...convert.hsl.rgb([value, 100, 50]), 255]),
+    () => colorUtil.toHexColor([...convert.hsl.rgb([value, 100, 50]), 255]),
     [value]
   );
+  const [rgbColor, setRgbColor] = React.useState(hexColor);
+  const { hslArr } = useColorPicker(rgbColor, setRgbColor);
+
+  useEffect(() => {
+    const [h] = hslArr;
+    if (h !== value) {
+      onChange(h);
+    }
+  }, [value, onChange, hslArr]);
+
   return (
     <Stack spacing={1}>
       <Stack direction="row" spacing={1}>
         <Typography variant="body2">{name}</Typography>
         <HelpTooltip description={description} />
       </Stack>
-      <HuePicker
-        color={hexColor}
-        // HSL is in [0-360, 0-100, 0-100]
-        onChangeComplete={({ hsl }) => onChange(hsl.h)}
+      <ColorPicker
+        value={hexColor}
+        onChange={setRgbColor}
+        hideColorTypeBtns={true}
+        hideOpacity={true}
+        hidePresets={true}
+        hideEyeDrop={true}
+        hideAdvancedSliders={true}
+        hideGradientType={true}
+        hideControls={true}
+        hideColorGuide={true}
+        hideInputs={true}
+        hidePickerSquare={true}
       />
     </Stack>
   );
