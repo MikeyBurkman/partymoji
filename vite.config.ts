bf8a0b17dd54a4e5373613ad3631d27fd3cc488a
Tom McKearney <tommck@gmail.com>
2025-04-06 12:55:40 -0400

    fixed top level await issue with prod build

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,13 @@ import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
 import wasm from "vite-plugin-wasm";
+import topLevelAwait from "vite-plugin-top-level-await";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths(), nodePolyfills(), wasm()],
+  plugins: [react(), tsconfigPaths(), nodePolyfills(), wasm(), topLevelAwait()],
   worker: {
-    plugins: () => [tsconfigPaths(), nodePolyfills(), wasm()],
+    format: 'es',
+    plugins: () => [tsconfigPaths(), nodePolyfills(), wasm(), topLevelAwait()],
   },
 });
