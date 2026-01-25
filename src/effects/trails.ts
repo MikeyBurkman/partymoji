bd19b7bb2384ba40dedb9c8429887b2d87647a04
Michael Burkman <michael.burkman@gmail.com>
2026-01-25 15:57:38 -0500

    Fix bug with trails effect

--- src/effects/trails.ts
+++ src/effects/trails.ts
@@ -49,8 +49,10 @@ export const trails = buildEffect({
     for (let i = 0; i < image.frames.length; i += 1) {
       const canvas = canvasUtil.createCanvas(image.dimensions);
       for (let n = numTrails; n > 0; n -= 1) {
-        const idx = i - n + 1;
+        const idx = Math.max(i - n + 1, 0);
         const frameIdx = idx >= 0 ? idx : image.frames.length + idx;
+
+        console.log('DATA', { n, idx, frameIdx });
 
         const frameToCopy = image.frames[frameIdx];
         canvasUtil.applyFilter(canvas, {
